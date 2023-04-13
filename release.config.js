dbb2e5dbbeb54c05269683b13073293713e3a26c
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-13 19:00:33 -0400

    feat: Adding Semantic Version on Main new token repo name

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "main",
-  repositoryUrl: "https://github.com/nelsonrp24/react-app",
+  repositoryUrl: "https://github.com/nelsonrp24/react-app.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
