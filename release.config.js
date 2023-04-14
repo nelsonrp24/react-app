b95545445f7899ab1e809417ae98cece8bd9f86d
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-13 21:18:22 -0400

    Adding zip

--- release.config.js
+++ release.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   branches: "main",
-  repositoryUrl: "https://github.com/nelsonrp24/react-app.git",
+  repositoryUrl: "https://github.com/nelsonrp24/react-app",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    ["@semantic-release/github", {
+        assets: [
+          { path: "build.zip", label:: "Build"},
+          { path: "coverage.zip", label:: "Coverage"}
+        ]
+
+    }]
   ]
 };
