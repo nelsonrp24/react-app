c3f3635a10608725b3ac65a54931e87b88399c70
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-13 21:20:29 -0400

    feat: adding ziping assets

--- release.config.js
+++ release.config.js
@@ -6,8 +6,8 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     ["@semantic-release/github", {
         assets: [
-          { path: "build.zip", label:: "Build"},
-          { path: "coverage.zip", label:: "Coverage"}
+          { path: "build.zip", label: "Build"},
+          { path: "coverage.zip", label: "Coverage"}
         ]
 
     }]
