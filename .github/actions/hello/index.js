1d3e91fbaeb0b58b246d19d9ff9011e4df5be275
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-19 18:13:40 -0400

    feat: core functionalities 1

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,17 +1,27 @@
-const core = require('@actions/core');
-const github =  require('@actions/github');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-    // throw(new Error("Some error message"));
-    const name = core.getInput('who-to-greet');
-    console.log(`Hello ${name}`);
-    const time = new Date();
-    core.setOutput("time", time.toString());
-    console.log(JSON.stringify(github, null, '\t'));
+  // throw(new Error("Some error message"));
 
-} catch(error) {
-    core.setFailed(error.message);
+  core.debug("Debug Message");
+  core.warning("Warning Message");
+  core.error("Error Message");
+
+  core.setSecret(name);
+
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toString());
+
+  core.startGroup("Logging github object");
+  console.log(JSON.stringify(github, null, "\t"));
+
+  core.endGroup();
+
+  core.exportVariable("HELLO", "heellllooooooo");
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-
