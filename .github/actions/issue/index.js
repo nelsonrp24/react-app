87ae25e5c4c0ec7ff2c2f5d2284a4d3fd321251b
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-24 16:08:07 -0400

    feat: feat: feat: adding action to create issue 4

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,27 +1,38 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  const token = core.getInput("token");
-  const title = core.getInput("title");
-  const body = core.getInput("body");
-  const assignees = core.getInput("assignees");
+async function run() {
 
-  const octokit = new github.GitHub(token);
+    try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
 
-  const response = octokit.issues.create({
-    //owner: github.context.repo.owner,
-    //repo: github.context.repo.repo,
-    ...github.context.repo, // JavaScript ESX syntax
-    //title: title --> how the key is the same as variable, we can just write de variable
-    title,
-    body,
-    assignees: assignees ? assignees.split("\n") : undefined
-  });
+    const octokit = new github.GitHub(token);
 
-  core.setOutput("issue", JSON.stringify(response.data));
-} catch (error) {
-  core.setFailed(error.message);
+    const response = await octokit.issues.create({
+        //owner: github.context.repo.owner,
+        //repo: github.context.repo.repo,
+        ...github.context.repo, // JavaScript ESX syntax
+        //title: title --> how the key is the same as variable, we can just write de variable
+        title,
+        body,
+        assignees: assignees ? assignees.split("\n") : undefined
+    });
+
+
+
+    core.setOutput("issue", JSON.stringify(response.data));
+
+    //core.setOutput("issue", "Fix...!!!");
+
+    } catch (error) {
+    core.setFailed(error.message);
+    }
+
 }
 
+run();
 // To compile: npx ncc build .github/actions/issue/index.js -o .github/actions/issue/dist
+
