76c88883f3bab235719d0073421ba4fe8fe2b2ac
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-24 16:09:49 -0400

    feat: feat: feat: adding action to create issue 5

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -4,28 +4,28 @@ const github = require("@actions/github");
 async function run() {
 
     try {
-    const token = core.getInput("token");
-    const title = core.getInput("title");
-    const body = core.getInput("body");
-    const assignees = core.getInput("assignees");
+    // const token = core.getInput("token");
+    // const title = core.getInput("title");
+    // const body = core.getInput("body");
+    // const assignees = core.getInput("assignees");
 
-    const octokit = new github.GitHub(token);
+    // const octokit = new github.GitHub(token);
 
-    const response = await octokit.issues.create({
-        //owner: github.context.repo.owner,
-        //repo: github.context.repo.repo,
-        ...github.context.repo, // JavaScript ESX syntax
-        //title: title --> how the key is the same as variable, we can just write de variable
-        title,
-        body,
-        assignees: assignees ? assignees.split("\n") : undefined
-    });
+    // const response = await octokit.issues.create({
+    //     //owner: github.context.repo.owner,
+    //     //repo: github.context.repo.repo,
+    //     ...github.context.repo, // JavaScript ESX syntax
+    //     //title: title --> how the key is the same as variable, we can just write de variable
+    //     title,
+    //     body,
+    //     assignees: assignees ? assignees.split("\n") : undefined
+    // });
 
 
 
-    core.setOutput("issue", JSON.stringify(response.data));
+    // core.setOutput("issue", JSON.stringify(response.data));
 
-    //core.setOutput("issue", "Fix...!!!");
+    core.setOutput("issue", "Fix...!!!");
 
     } catch (error) {
     core.setFailed(error.message);
