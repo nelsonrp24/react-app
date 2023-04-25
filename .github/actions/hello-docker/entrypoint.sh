5ccecdb9f9bfa11fd02a4592a0adea1da88e8d4f
Nelson Perez <nelsonf_perez@unicomer.com>
2023-04-25 11:49:32 -0400

    feat: docker actions 1

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+
+echo "::debug :: Debug Message"
+
+echo "::warning :: Warning Message"
+echo "::error :: Error Message"
+echo "::info :: Info Message"
+
+echo "::add-mask:: $1"
+
+echo -n "::info :: Esto esta enmascarado:  "
+echo "::add-mask:: Hello World....!!!!! "
+
+echo "Hello $1"
+
+time=$(date)
+
+echo "::set-output name=time::$time"
+
+echo "::group::Some Expandable logs"
+echo "some stuff 1"
+echo "some stuff 2"
+echo "some stuff 3"
+echo "::endgroup"
+
+
+echo "::set-env name=HELLO:: thisIsAGreeting"
